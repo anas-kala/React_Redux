dbfc5d9bf90105c108a86d1a6997e19c46c86171
Anas Al Kala <aal1580@students.fhv.at>
2021-12-15 09:58:10 +0100

    adding details component and wrapping it with a redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 // Action creator
+// note that we did not use export default here, but rather a "name export". which means that importing this function in another component will be through {selectSong}
 export const selectSong = (song) => {
     // return an action
     return {
--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
     renderList() {
         return this.props.songs.map((song) => {
             return (
                 <div className='item' key={song.title}>
-                    <div className='right floated content'>
+                    <div className='right floated content' onClick={() => this.props.selectSong(song)}>
                         <button className='ui button primary'>
                             select
                         </button>
@@ -31,4 +32,8 @@ const mapStateToProps = (state) => {
     return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+// the first argument in the connect method is the props that are going to be passed to this component.
+// the second argument is the action that is going to be passed to the dispatcher which will update the state in the reducer.
+export default connect(mapStateToProps, {
+    selectSong: selectSong
+})(SongList);
